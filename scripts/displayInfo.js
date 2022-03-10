ea6aa05da6066d5fbf2ea9282b974e62ec6cca49
TShelby1 <94877568+TShelby1@users.noreply.github.com>
2022-03-10 14:20:46 -0800

    last touches
    got the alerts

--- scripts/displayInfo.js
+++ scripts/displayInfo.js
@@ -1,7 +1,7 @@
 console.log("Script");
 // Create an obj literal for the pat saloon
 const petSaloon={
-    name:"The Fashion Pet",
+    names:"The Fashion Pet",
     address:{
         street:"ave univsersity",
         number:"2622",
@@ -16,7 +16,7 @@ const petSaloon={
     //create pets array 
     pets:[
         {  //first pet
-            name:"bob",
+            name:"Bob",
             age:50,
             gender:"Male",
             breed:"Dane",
@@ -26,7 +26,7 @@ const petSaloon={
             
         },
         {  
-            name:"tom",
+            name:"Tom",
             age:50,
             gender:"Male",
             breed:"chihuahua",
@@ -36,7 +36,7 @@ const petSaloon={
             
         },
         {  
-            name:"george",
+            name:"George",
             age:50,
             gender:"Male",
             breed:"heeler",
@@ -46,7 +46,7 @@ const petSaloon={
             
         },
         {  
-            name:"ox",
+            name:"Ox",
             age:50,
             gender:"Male",
             breed:"golden retriever",
@@ -56,7 +56,7 @@ const petSaloon={
             
         },
         {  
-            name:"gizmo",
+            name:"Gizmo",
             age:50,
             gender:"Male",
             breed:"lab",
@@ -67,21 +67,29 @@ const petSaloon={
         }
     ]
 }
-console.log(petSaloon.address.zip);
-console.log(petSaloon.pets.length);
-// console.log(petSaloon.pets[0].service);
-
-
-
 
 
 //display the pet saloon info in the footer section of the html
 function displaySaloonInfo(){
-document.getElementById("footer-info").innerHTML=`Welcome ${petSaloon.name}... in ${petSaloon.address.city}`;
+document.getElementById("footer-info").innerHTML=petSaloon.names;
 }
 displaySaloonInfo();
 
 function displayPetNames(){
-document.getElementById("petNames").innerHTML=`${petSaloon.pets[3].service}`;
+    console.log("Register");
+    let name= prompt('Enter new pet name');
+    let age=prompt('Enter pet age');
+    let gender=prompt('Enter pet gender');
+    let breed=prompt('Enter pet breed');
+    let service=prompt('Enter service');
+    let ownerName=prompt('Enter owner name');
+    let contactPhone=prompt('Enter contact phone');
+    petSaloon.pets.push({name, age, gender, breed, service, ownerName, contactPhone});
+    document.getElementById("petNames").innerHTML="";
+    for(let i=0; i<petSaloon.pets.length; i++){
+        document.getElementById("petNames").innerHTML+=`<li id=${i}"><p>Pet Name: ${petSaloon.pets[i].name}</p><p> Pet Gender: ${petSaloon.pets[i].gender}</p><p> Pet Breed: ${petSaloon.pets[i].breed}</p><p> Pet Service: ${petSaloon.pets[i].service}</p><p> Pet Owner Name: ${petSaloon.pets[i].ownerName}</p><p> Pet Contact Phone Number: ${petSaloon.pets[i].contactPhone}</p> </li>`
+    }
 }
 displayPetNames();
+
+alert('this many animals in array' +  petSaloon.pets.length);
