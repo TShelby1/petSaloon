176e0e1c49a1c4c3af82f94d8d1fcef6b2ea09c3
TShelby1 <94877568+TShelby1@users.noreply.github.com>
2022-03-13 11:58:38 -0700

    displaying pets in the html
    this verion displays pets in the html and uses bootstrap for the CSS styling

--- scripts/displayInfo.js
+++ scripts/displayInfo.js
@@ -18,17 +18,13 @@ const petSaloon={
 }
 
 
-//display the pet saloon info in the footer section of the html
-// function displaySaloonInfo(){
-// document.getElementById("footer-info").innerHTML=petSaloon.names;
-// }
-// displaySaloonInfo();
+// display the pet saloon info in the footer section of the html
+function displaySaloonInfo(){
+document.getElementById("footer-info").innerHTML;
+}
+displaySaloonInfo();
 
-function displayPetNames(){
-    // document.getElementById("petNames").innerHTML="petSaloo;
-    for(let i=0; i<petSaloon.pets.length; i++){
-        document.getElementById("petNames").innerHTML+=`<li id=${i}"><p>Pet Name: ${petSaloon.pets[i].petName}</p><p> Pet Gender: ${petSaloon.pets[i].petGender}</p><p> Pet Breed: ${petSaloon.pets[i].petBreed}</p><p> Pet Service: ${petSaloon.pets[i].petService}</p><p> Pet Owner Name: ${petSaloon.pets[i].petOwnerName}</p><p> Pet Contact Phone Number: ${petSaloon.pets[i].petContactPhone}</p> </li>`
-    }
-}console.log(petSaloon.pets)
-displayPetNames();
+
+    
+
 // alert('this many animals in array' +  petSaloon.pets.length);
--- scripts/register.js
+++ scripts/register.js
@@ -17,23 +17,80 @@ let inputPetService= document.getElementById("txtPetService");
 let inputPetOwnerName= document.getElementById("txtPetOwnerName");
 let inputPetContactPhone= document.getElementById("txtPetContactPhone");
 
-// create some pets
-let scooby = new Pet("Scooby",50,"sdfsdf","dfgdfg","sdfgdfgdfg","sdfsdf","7609876543");
-let scrappy = new Pet("scrappy",50,"dfgdfg","tyutyu","hjkljkl","sdfsdf","1234567");
 
-petSaloon.pets.push(scooby,scrappy);
 
-// console.log(petSaloon.pets);
+
+function isValid(aPet){
+    // return false when the pet is not valid
+    // return true if the pet is valid
+    let valid=true;
+    // return the error in the console
+    if(aPet.petName.length==0){
+        // if we get here it means that the name is not valid
+        valid=false;
+        console.log("Invalid Name")
+    }
+    if(aPet.petAge.length==0){
+        // if we get here it means that the name is not valid
+        valid=false;
+        console.log("Invalid Pet Age")
+    }
+    if(aPet.petService.length==0){
+        // if we get here it means that the name is not valid
+        valid=false;
+        console.log("Invalid Pet Service")
+    }
+    if(aPet.petContactPhone.length==0){
+        // if we get here it means that the name is not valid
+        valid=false;
+        console.log("Invalid Cntact Phone")
+    }
+    // validations
+
+    return valid;
+}
 
 function register(){
     console.log(inputPetName.value,inputPetAge.value, inputPetGender.value, inputPetBreed.value, inputPetService.value, inputPetOwnerName.value, inputPetContactPhone.value);
-    // create the object
+    
     let thePet = new Pet(inputPetName.value, inputPetAge.value, inputPetGender.value, inputPetBreed.value, inputPetService.value, inputPetOwnerName.value, inputPetContactPhone.value);
 
-    console.log(thePet)
-    // push the object
-    petSaloon.pets.push(thePet);
-    //display the object in the console
-}
-console.log(petSaloon.pets)
+    console.log(thePet);
 
+    if(isValid(thePet)){
+        petSaloon.pets.push(thePet);
+    // display object in html
+        displayPets();
+    }
+
+}
+
+
+// steps are to travel the array, create the template, then display the tmp on the html
+function displayPets(){
+    let tmp="";
+    // travel the array 
+    for(let i=0; i<petSaloon.pets.length; i++){
+        // create the template
+        tmp+=`
+        <div class="pet">
+            <h4> Pet Name:  ${petSaloon.pets[i].petName} 🐾</h4>
+            <p> Pet Gender:  ${petSaloon.pets[i].petGender}</p>
+            <p> Pet Breed: ${petSaloon.pets[i].petBreed}</p>
+            <p> Pet Service: ${petSaloon.pets[i].petService}</p>
+            <p> Pet Owner Name: ${petSaloon.pets[i].petOwnerName}</p>
+            <p> Pet Contact Phone Number: ${petSaloon.pets[i].petContactPhone}</p>
+        </div>`;
+        
+    }
+    document.getElementById("pets").innerHTML=tmp;
+}
+
+function init(){
+    // create some pets
+    let scooby = new Pet("Scooby",50,"male","goldie","worms","tom","7609876543");
+    let scrappy = new Pet("scrappy",50,"female","lab","nails","jeff","1234567");
+    petSaloon.pets.push(scooby,scrappy);
+    displayPets();
+}
+window.onload=init;
